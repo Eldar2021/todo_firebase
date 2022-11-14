5be003a0d6e88590c7b478423ba110326b466816
Eldar2021 <eldiiaralmazbekov@gmail.com>
2022-11-15 00:39:37 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'app/app.dart';
+import 'firebase_options.dart';
+
+// firebase cli
+
+// flutterfire_cli
+
+// https://firebase.google.com/docs/flutter/setup?authuser=0&hl=en&platform=ios 
+// flutter di firebasege bailady
+
+// https://firebase.google.com/docs/firestore/quickstart?authuser=0&hl=en
+// read data
+// add data
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
